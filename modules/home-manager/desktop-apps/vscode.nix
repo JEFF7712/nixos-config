394af620aa5e00a4a9c0bf01a02a3f1e5fec5612
JEFF7712 <rupanpandyan@gmail.com>
2025-12-05 04:53:08 -0600

    Update

--- modules/home-manager/desktop-apps/vscode.nix
+++ modules/home-manager/desktop-apps/vscode.nix
@@ -6,17 +6,19 @@
       enable = true;
       mutableExtensionsDir = false; 
       
-      extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix       # Nix syntax highlighting
-        ms-python.python   # Python support
-        github.copilot     # AI
-        eamodio.gitlens    # Git supercharger
-      ];
-      
-      userSettings = {
-        "editor.fontFamily" = "'JetBrainsMono Nerd Font', 'Droid Sans Mono', 'monospace'";
-        "editor.fontSize" = 14;
-        "window.titleBarStyle" = "custom";
+      profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+          bbenoist.nix       # Nix syntax highlighting
+          ms-python.python   # Python support
+          github.copilot     # AI
+          eamodio.gitlens    # Git supercharger
+        ];
+        
+        userSettings = {
+          "editor.fontFamily" = "'JetBrainsMono Nerd Font', 'Droid Sans Mono', 'monospace'";
+          "editor.fontSize" = 14;
+          "window.titleBarStyle" = "custom";
+        };
       };
     };
   };
--- modules/home-manager/terminals/kitty.nix
+++ modules/home-manager/terminals/kitty.nix
@@ -2,8 +2,9 @@
   options.kitty.enable = lib.mkEnableOption "kitty terminal";
 
   config = lib.mkIf config.kitty.enable {
-    home.packages = [ pkgs.kitty ];
-
-    xdg.configFile."kitty".source = ../configs/kitty;
+    programs.kitty = {
+      enable = true;
+      extraConfig = builtins.readFile ../configs/kitty/kitty.conf;
+    };
   };
 }
