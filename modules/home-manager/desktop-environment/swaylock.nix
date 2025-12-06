0494ace2f131e72f5c21ce17d98055d08c21550c
JEFF7712 <rupanpandyan@gmail.com>
2025-12-06 02:50:50 -0600

    Updates

--- modules/home-manager/desktop-environment/swaylock.nix
+++ modules/home-manager/desktop-environment/swaylock.nix
@@ -6,20 +6,32 @@
       enable = true;
       package = pkgs.swaylock-effects;
       settings = {
-        clock = true;
-        datestr = "%a, %B %e";
-        timestr = "%k:%M";
         screenshots = true;
         fade-in = 0.2;
-      
-        # Visual Ring Config
         indicator = true;
-        indicator-radius = 100;
-        indicator-thickness = 7;
         effect-blur = "7x5";
         effect-vignette = "0.5:0.5";
-        ring-color = "3b4252";
-        key-hl-color = "88c0d0";
+  	indicator-radius = 160; 
+  	indicator-thickness = 20; 
+  	inside-color = "00000000"; 
+  	inside-clear-color = "00000000"; 
+  	inside-ver-color = "00000000"; 
+  	inside-wrong-color = "00000000"; 
+  	key-hl-color = "ffffff"; 
+  	bs-hl-color = "ffffff"; 
+  	ring-color = "101010"; 
+  	ring-wrong-color = "c50000"; 
+  	ring-ver-color = "1db954"; 
+  	line-uses-ring = true; 
+  	line-color = "00000000"; 
+  	font = "JetBrainsMono Nerd Font"; 
+	font-size = 40;  	
+  	text-color = "00000000"; 
+  	text-clear-color = "00000000"; 
+  	text-wrong-color = "00000000"; 
+  	text-ver-color = "00000000"; 
+  	separator-color = "00000000"; 
+
       };
     };
 
