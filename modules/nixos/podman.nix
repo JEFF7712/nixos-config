540c81f9cc88d444a1316612c63ce67c98bf61d4
JEFF7712 <rupanpandyan@gmail.com>
2025-12-06 03:38:51 -0600

    Updates

---
+++ modules/nixos/podman.nix
@@ -0,0 +1,12 @@
+{ pkgs, lib, config, ... }: {
+  options.podman.enable = lib.mkEnableOption "podman";
+
+  config = lib.mkIf config.podman.enable {
+
+    virtualisation.podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+}
