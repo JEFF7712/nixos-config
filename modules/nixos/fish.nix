bc55a3623d0b6bd13450b09e3ae15c98c7371a6e
JEFF7712 <rupanpandyan@gmail.com>
2025-12-07 21:51:33 -0600

    Updates

--- modules/nixos/fish.nix
+++ modules/nixos/fish.nix
@@ -2,7 +2,15 @@
   options.fish.enable = lib.mkEnableOption "fish";
 
   config = lib.mkIf config.fish.enable {
-    programs.fish.enable = true;
+    programs.fish = {
+      enable = true;
+      shellAliases = {
+        gc = "nix-collect-garbage -d";
+        bnix="cd $HOME/nixos && git add . && sudo nixos-rebuild switch --flake .#laptop && git commit -m 'Updates' && git push";
+        cniri="sudo $EDITOR $HOME/nixos/modules/home-manager/configs/niri/config.kdl";
+      };
+    };
+
     users.users.rupan.shell = pkgs.fish;
   };
 }
