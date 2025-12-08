5bdcb0aa08e2006fe89881aa7a694aecd0cb544d
JEFF7712 <rupanpandyan@gmail.com>
2025-12-07 22:24:11 -0600

    Updates

--- modules/nixos/fish.nix
+++ modules/nixos/fish.nix
@@ -4,11 +4,22 @@
   config = lib.mkIf config.fish.enable {
     programs.fish = {
       enable = true;
+
       shellAliases = {
         gc = "nix-collect-garbage -d";
         bnix="cd $HOME/nixos && git add . && sudo nixos-rebuild switch --flake .#laptop && git commit -m 'Updates' && git push";
         cniri="sudo $EDITOR $HOME/nixos/modules/home-manager/configs/niri/config.kdl";
+        ls = "eza --icons";      
+        ll = "eza -l --icons";   
+        lt = "eza --tree --level=2 --icons"; 
+        la = "eza -a --icons";     
+        lla = "eza -la --icons"; 
       };
+
+      interactiveShellInit = ''
+      set -g fish_color_param purple
+      set -g fish_color_valid_path purple
+      '';
     };
 
     programs.starship = {
@@ -31,6 +42,9 @@
       };
     };
 
+    environment.systemPackages = [ pkgs.eza ];
+
+    environment.variables.LS_COLORS = "di=01;35";
     users.users.rupan.shell = pkgs.fish;
   };
 }
