bb2d54add0a052a9b3da6eb848fd5229c56efdbe
JEFF7712 <rupanpandyan@gmail.com>
2025-12-07 20:52:23 -0600

    Updates

--- modules/home-manager/desktop-environment/waypaper.nix
+++ modules/home-manager/desktop-environment/waypaper.nix
@@ -5,6 +5,29 @@
     home.packages = with pkgs; [
       waypaper
     ];
-    
+
+    systemd.user.services.wallpaper-cycler = {
+      Unit = {
+        Description = "Cycle wallpaper randomly using waypaper";
+        After = [ "graphical-session.target" ];
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.waypaper}/bin/waypaper --random";
+      };
+    };
+
+    systemd.user.timers.wallpaper-cycler = {
+      Unit = {
+        Description = "Timer to cycle wallpaper every hour";
+      };
+      Timer = {
+        OnBootSec = "5m"; 
+        OnUnitActiveSec = "1h"; 
+      };
+      Install = {
+        WantedBy = [ "timers.target" ];
+      };
+    };
   };
 }
