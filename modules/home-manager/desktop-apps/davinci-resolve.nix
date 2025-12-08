8f30816ac57bb940ab432bffc169f435d1062cda
JEFF7712 <rupanpandyan@gmail.com>
2025-12-08 02:45:50 -0600

    Updates

--- modules/home-manager/desktop-apps/davinci-resolve.nix
+++ modules/home-manager/desktop-apps/davinci-resolve.nix
@@ -7,7 +7,6 @@
         buildFHSEnv = args: pkgs.buildFHSEnv (args // {
           extraBwrapArgs = (args.extraBwrapArgs or []) ++ [
             "--setenv QT_QPA_PLATFORM xcb"
-            "--setenv LD_PRELOAD /usr/lib/libglib-2.0.so.0 /usr/lib/libgio-2.0.so.0 /usr/lib/libgmodule-2.0.so.0"
           ];
         });
       }))
