341333b521bb71601716fbf8f5b06399a075dbbe
JEFF7712 <rupanpandyan@gmail.com>
2025-12-08 02:36:24 -0600

    Updates

--- modules/home-manager/desktop-apps/davinci-resolve.nix
+++ modules/home-manager/desktop-apps/davinci-resolve.nix
@@ -3,7 +3,13 @@
 
   config = lib.mkIf config.davinci-resolve.enable {
     home.packages = with pkgs; [
-      davinci-resolve
-    ];   
+      (davinci-resolve.override (old: {
+        buildFHSEnv = args: pkgs.buildFHSEnv (args // {
+          extraBwrapArgs = (args.extraBwrapArgs or []) ++ [
+            "--setenv QT_QPA_PLATFORM xcb"
+          ];
+        });
+      }))
+    ];    
   };
 }
