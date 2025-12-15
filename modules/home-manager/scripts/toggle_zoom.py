ef66e678b4446f232465db85d641b9843946304a
JEFF7712 <rupanpandyan@gmail.com>
2025-12-15 16:03:03 -0600

    Updates

---
+++ modules/home-manager/scripts/toggle_zoom.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from marionette_driver.marionette import Marionette
+
+client = Marionette(host='localhost', port=2828)
+client.start_session()
+
+with client.using_context("chrome"):
+    current_val = client.execute_script("""
+        return Services.prefs.getCharPref('layout.css.devPixelsPerPx', '-1.0');
+    """)
+
+    new_val = "2.0" if current_val == "1.0" else "1.0"
+    
+    client.execute_script(f"""
+        Services.prefs.setCharPref('layout.css.devPixelsPerPx', '{new_val}');
+    """)
+
+client.delete_session()
