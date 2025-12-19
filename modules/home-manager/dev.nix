42868214f272c87056948f701e25aa19978d1d44
JEFF7712 <rupanpandyan@gmail.com>
2025-12-19 01:13:58 -0600

    Updates

--- modules/home-manager/dev.nix
+++ modules/home-manager/dev.nix
@@ -9,8 +9,17 @@
     home.packages = with pkgs; [
       terraform
       ansible
-      talosctl
       kubectl
+      (pkgs.runCommand "talosctl-1.11.6" {
+        src = pkgs.fetchurl {
+          url = "https://github.com/siderolabs/talos/releases/download/v1.11.6/talosctl-linux-amd64";
+          sha256 = "0d6gql2wm54cp8pqxr8m6lvffql8im6y3rl1680hiawwbxffyj52";
+        };
+      } ''
+        mkdir -p $out/bin
+        cp $src $out/bin/talosctl
+        chmod +x $out/bin/talosctl
+      '')
     ];
   };
 }
