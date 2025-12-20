6fffc3eba9708e9411498666a6fd3a9ae35dfcca
JEFF7712 <rupanpandyan@gmail.com>
2025-12-20 02:14:32 -0600

    Updates

--- modules/home-manager/niri.nix
+++ modules/home-manager/niri.nix
@@ -444,56 +444,6 @@
       enableFishIntegration = true;
     };
 
-    programs.swaylock = {
-      enable = true;
-      package = pkgs.swaylock-effects;
-      settings = {
-        screenshots = true;
-        indicator = true;
-	      effect-scale = 0.5;  
-        effect-blur = "7x4";
-        effect-vignette = "0.1:0.3";
-        indicator-radius = 100; 
-        indicator-thickness = 10; 
-        inside-color = "00000000"; 
-        inside-clear-color = "00000000"; 
-        inside-ver-color = "00000000"; 
-        inside-wrong-color = "00000000"; 
-        key-hl-color = "ffffff"; 
-        bs-hl-color = "ffffff"; 
-        ring-color = "101010"; 
-        ring-wrong-color = "c50000"; 
-        ring-ver-color = "1db954"; 
-        line-uses-ring = true; 
-        line-color = "00000000"; 
-        font = "JetBrainsMono Nerd Font"; 
-        font-size = 40;  	
-        text-color = "00000000"; 
-        text-clear-color = "00000000"; 
-        text-wrong-color = "00000000"; 
-        text-ver-color = "00000000"; 
-        separator-color = "00000000"; 
-      };
-    };
-
-    services.swayidle = {
-      enable = true;
-      events = {
-        before-sleep = "${pkgs.swaylock-effects}/bin/swaylock -fF";
-        lock = "${pkgs.swaylock-effects}/bin/swaylock -fF";
-      };
-      timeouts = [
-        { 
-          timeout = 300; 
-          command = "${pkgs.swaylock-effects}/bin/swaylock -fF"; 
-        }
-        {
-          timeout = 600;
-          command = "${pkgs.systemd}/bin/systemctl suspend";
-        }
-      ];
-    };
-
     systemd.user.services.swww = {
       Unit = {
         Description = "Wayland wallpaper daemon";
