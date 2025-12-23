06da5a35643c855e7a70413d369de5608c18e7e1
JEFF7712 <rupanpandyan@gmail.com>
2025-12-22 22:00:46 -0600

    Updates

--- flake.nix
+++ flake.nix
@@ -76,20 +76,24 @@
         homelab = pkgs.mkShell {
           packages = with pkgs; [ 
             kubectl
-            (pkgs.runCommand "talosctl-1.11.6" {
-              src = pkgs.fetchurl {
-                url = "https://github.com/siderolabs/talos/releases/download/v1.11.6/talosctl-linux-amd64";
-                sha256 = "0d6gql2wm54cp8pqxr8m6lvffql8im6y3rl1680hiawwbxffyj52";
-              };
-            } ''
-              mkdir -p $out/bin
-              cp $src $out/bin/talosctl
-              chmod +x $out/bin/talosctl
-            '')
             terraform
             terragrunt
             ansible
             fluxcd
+            (stdenv.mkDerivation {
+              pname = "talosctl";
+              version = "1.11.6";
+              src = fetchurl {
+                url = "https://github.com/siderolabs/talos/releases/download/v1.11.6/talosctl-linux-amd64";
+                hash = "sha256-0d6gql2wm54cp8pqxr8m6lvffql8im6y3rl1680hiawwbxffyj52="; # Ensure SRI format
+              };
+              phases = [ "installPhase" ];
+              installPhase = ''
+                mkdir -p $out/bin
+                cp $src $out/bin/talosctl
+                chmod +x $out/bin/talosctl
+              '';
+            })
           ];
           shellHook = ''echo "Welcome to the homelab Development Shell."'';
         };
