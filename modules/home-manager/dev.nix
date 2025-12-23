b95d1cdd68cb65e3f02a5be7eb82457076fe876f
JEFF7712 <rupanpandyan@gmail.com>
2025-12-22 21:55:28 -0600

    Updates

--- modules/home-manager/dev.nix
+++ modules/home-manager/dev.nix
@@ -7,21 +7,6 @@
       nix-direnv.enable = true;
     };
     home.packages = with pkgs; [
-      terraform
-      ansible
-      kubectl
-      terragrunt
-      (pkgs.runCommand "talosctl-1.11.6" {
-        src = pkgs.fetchurl {
-          url = "https://github.com/siderolabs/talos/releases/download/v1.11.6/talosctl-linux-amd64";
-          sha256 = "0d6gql2wm54cp8pqxr8m6lvffql8im6y3rl1680hiawwbxffyj52";
-        };
-      } ''
-        mkdir -p $out/bin
-        cp $src $out/bin/talosctl
-        chmod +x $out/bin/talosctl
-      '')
-      fluxcd
       gemini-cli
       geminicommit
     ];
