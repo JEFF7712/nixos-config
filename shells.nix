8e70bfd924cf6132661db31ea18267c7cf21fcd6
JEFF7712 <rupanpandyan@gmail.com>
2025-12-25 22:27:13 -0600

    Updates

--- shells.nix
+++ shells.nix
@@ -13,16 +13,7 @@
       terragrunt
       ansible
       fluxcd
-      (stdenv.mkDerivation {
-        pname = "talosctl";
-        version = "1.11.6";
-        src = fetchurl {
-          url = "https://github.com/siderolabs/talos/releases/download/v1.11.6/talosctl-linux-amd64";
-          sha256 = "0d6gql2wm54cp8pqxr8m6lvffql8im6y3rl1680hiawwbxffyj52";
-        };
-        phases = [ "installPhase" ];
-        installPhase = "mkdir -p $out/bin; cp $src $out/bin/talosctl; chmod +x $out/bin/talosctl";
-      })
+      talosctl
       cilium-cli
     ];
     shellHook = ''echo "Welcome to the homelab Development Shell."'';
