fb0319a3f8b78bd362706abdbaffca25853b536f
JEFF7712 <rupanpandyan@gmail.com>
2025-12-28 20:15:26 -0600

    Updates

---
+++ modules/nixos/game.nix
@@ -0,0 +1,23 @@
+{ pkgs, lib, config, ... }: {
+
+  options.game.enable = lib.mkEnableOption "game";
+  config = lib.mkIf config.game.enable {
+
+    programs.gamemode.enable = true;
+
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+
+
+    environment.systemPackages = with pkgs; [
+      mangohud
+      protonup-qt
+      lutris
+      heroic
+      wineWow64Packages.stable
+    ];
+  };
+}
