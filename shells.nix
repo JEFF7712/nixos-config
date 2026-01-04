4c4fef9f558f2562554a5846d540485f8dcecc6b
JEFF7712 <rupanpandyan@gmail.com>
2026-01-03 18:05:32 -0600

    Updates

--- shells.nix
+++ shells.nix
@@ -1,9 +1,32 @@
-{ pkgs, pythonEnv }:
+{ pkgs, CBEpythonEnv, pythonEnv }:
 
 {
   cbe = pkgs.mkShell {
-    packages = [ pythonEnv pkgs.openblas ];
+    packages = [ CBEpythonEnv pkgs.openblas ];
     shellHook = ''echo "Welcome to the CBE Development Shell."'';
+  };
+
+  python = pkgs.mkShell {
+    packages = [
+      pythonEnv
+      pkgs.python3Packages.pip
+      pkgs.python3Packages.virtualenv
+      pkgs.cudaPackages.cudatoolkit
+      pkgs.cudaPackages.cudnn
+      pkgs.linuxPackages.nvidia_x11
+      pkgs.libGL
+      pkgs.stdenv.cc.cc.lib
+    ];
+
+    shellHook = ''
+      export PIP_PREFIX="$(pwd)/.build/pip_packages"
+      export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+      export PATH="$PIP_PREFIX/bin:$PATH"
+      export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+      export LD_LIBRARY_PATH=/run/opengl-driver/lib:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.libGL}/lib:$LD_LIBRARY_PATH
+      unset SOURCE_DATE_EPOCH
+      echo "Welcome to the Python Development Shell."
+    '';
   };
 
   homelab = pkgs.mkShell {
