e6ef28b5be065b4d61469951346add4cf95a7957
JEFF7712 <rupanpandyan@gmail.com>
2026-01-03 23:18:07 -0600

    Updates

--- shells.nix
+++ shells.nix
@@ -7,12 +7,10 @@
   };
 
   python = pkgs.mkShell {
-    packages = [
+packages = [
       pythonEnv
       pkgs.python3Packages.pip
       pkgs.python3Packages.virtualenv
-      pkgs.cudaPackages.cudatoolkit
-      pkgs.cudaPackages.cudnn
       pkgs.linuxPackages.nvidia_x11
       pkgs.libGL
       pkgs.stdenv.cc.cc.lib
@@ -23,10 +21,9 @@
       export PIP_PREFIX="$(pwd)/.build/pip_packages"
       export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
       export PATH="$PIP_PREFIX/bin:$PATH"
-      export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
-      export LD_LIBRARY_PATH="/run/opengl-driver/lib:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.cudaPackages.cudatoolkit}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.libGL}/lib:$LD_LIBRARY_PATH"
-      unset SOURCE_DATE_EPOCH # Fixes some pip install issues
-      echo "Welcome to the Python Development Shell."
+      export LD_LIBRARY_PATH="/run/opengl-driver/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.libGL}/lib:$LD_LIBRARY_PATH"
+      unset SOURCE_DATE_EPOCH
+      echo "PyTorch Shell"
     '';
   };
 
