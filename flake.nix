24ddd4f5fbb8cadc290c5cf1f4f45595aaee6308
JEFF7712 <rupanpandyan@gmail.com>
2026-01-16 18:47:46 -0600

    Updates

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,7 +19,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-vscode-extensions, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, nix-vscode-extensions, ... }@inputs:
     let
       system = "x86_64-linux";
 
@@ -28,16 +29,21 @@
         overlays = [ nix-vscode-extensions.overlays.default ];
       };
 
+      pkgs-stable = import nixpkgs-stable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
       mkSystem = host: userModule: nixpkgs.lib.nixosSystem {
         inherit system pkgs;
-        specialArgs = { inherit inputs; };
+        specialArgs = { inherit inputs pkgs-stable; };
         modules = [
           ./hosts/${host}/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.extraSpecialArgs = { inherit inputs pkgs-stable; };
             home-manager.backupFileExtension = "backup";
             home-manager.users.rupan = import userModule;
           }
