bc5321f7d10379fe2b0356ef2b6bbaa4e04c65df
JEFF7712 <rupanpandyan@gmail.com>
2026-02-27 01:12:33 -0600

    Updates

--- modules/nixos/waydroid.nix
+++ modules/nixos/waydroid.nix
@@ -4,7 +4,8 @@
   options.waydroid.enable = lib.mkEnableOption "waydroid";
 
   config = lib.mkIf config.waydroid.enable {
-    environment.systemPackages = with pkgs; [ waydroid ];
+    environment.systemPackages = with pkgs; [ waydroid android-tools ];
     virtualisation.waydroid.enable = true;
+    users.users.rupan.extraGroups = [ "kvm" "adbusers" ];
   };
 }
