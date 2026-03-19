dc4e4d1478dc91917fbb8067154c0815ef73ab60
JEFF7712 <rupanpandyan@gmail.com>
2026-03-18 20:39:17 -0500

    Updates

--- modules/nixos/git.nix
+++ modules/nixos/git.nix
@@ -4,6 +4,16 @@
   options.git.enable = lib.mkEnableOption "git";
 
   config = lib.mkIf config.git.enable {
-    programs.git.enable = true;
+    programs.git = {
+      enable = true;
+      config = {
+        user.name = "JEFF7712";
+        user.email = "rupanpandyan@gmail.com";
+        credential."https://github.com".helper = "!gh auth git-credential";
+        credential."https://gist.github.com".helper = "!gh auth git-credential";
+        init.defaultBranch = "main";
+        safe.directory = "/home/rupan/nixos";
+      };
+    };
   };
 }
