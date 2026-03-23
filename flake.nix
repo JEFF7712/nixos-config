9f0ac98ad073e4f34f18057bcdb1f3c4bdc68272
JEFF7712 <rupanpandyan@gmail.com>
2026-03-22 23:12:37 -0500

    feat: migrate flake.nix to flake-parts with import-tree inputs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -38,47 +39,51 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, nix-vscode-extensions, nixvim, globalprotect-openconnect,... }@inputs:
-    let
-      system = "x86_64-linux";
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, nix-vscode-extensions, nixvim, globalprotect-openconnect, ... }@inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        config.permittedInsecurePackages = [
-          "electron-37.10.3"
-        ];
-        overlays = [
-          nix-vscode-extensions.overlays.default
-          inputs.niri-blur.overlays.default
-        ];
-      };
+      flake = let
+        system = "x86_64-linux";
 
-      pkgs-stable = import nixpkgs-stable {
-        inherit system;
-        config.allowUnfree = true;
-      };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.permittedInsecurePackages = [
+            "electron-37.10.3"
+          ];
+          overlays = [
+            nix-vscode-extensions.overlays.default
+            inputs.niri-blur.overlays.default
+          ];
+        };
 
-      mkSystem = host: userModule: nixpkgs.lib.nixosSystem {
-        inherit system pkgs;
-        specialArgs = { inherit inputs pkgs-stable; };
-        modules = [
-          ./hosts/${host}/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs pkgs-stable; };
-            home-manager.backupFileExtension = "backup";
-            home-manager.users.rupan = import userModule;
-          }
-        ];
-      };
+        pkgs-stable = import nixpkgs-stable {
+          inherit system;
+          config.allowUnfree = true;
+        };
 
-    in {
-      nixosConfigurations = {
-        laptop = mkSystem "laptop" ./home/rupan/laptop.nix;
-        iso = mkSystem "iso" ./home/rupan/laptop.nix;
+        mkSystem = host: userModule: nixpkgs.lib.nixosSystem {
+          inherit system pkgs;
+          specialArgs = { inherit inputs pkgs-stable; };
+          modules = [
+            ./hosts/${host}/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs pkgs-stable; };
+              home-manager.backupFileExtension = "backup";
+              home-manager.users.rupan = import userModule;
+            }
+          ];
+        };
+
+      in {
+        nixosConfigurations = {
+          laptop = mkSystem "laptop" ./home/rupan/laptop.nix;
+          iso = mkSystem "iso" ./home/rupan/laptop.nix;
+        };
       };
-  };
+    };
 }
