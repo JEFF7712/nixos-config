aef0767fa55142a33fd65ec75f068cfd4fd808b7
JEFF7712 <rupanpandyan@gmail.com>
2026-04-30 01:37:53 -0500

    style(flake): fix indentation of python overrides

--- flake.nix
+++ flake.nix
@@ -75,23 +75,22 @@
           aioboto3TestFixOverlay =
             final: prev:
             let
-              pythonOverrides =
-                pyFinal: pyPrev: {
-                  aioboto3 = pyPrev.aioboto3.overridePythonAttrs (old: {
-                    disabledTests = (old.disabledTests or [ ]) ++ [
-                      "test_dynamo_resource_query"
-                      "test_dynamo_resource_put"
-                      "test_dynamo_resource_batch_write_flush_on_exit_context"
-                      "test_dynamo_resource_batch_write_flush_amount"
-                      "test_flush_doesnt_reset_item_buffer"
-                      "test_dynamo_resource_property"
-                      "test_dynamo_resource_waiter"
-                    ];
-                  });
-                  fastmcp = pyPrev.fastmcp.overridePythonAttrs (old: {
-                    doCheck = false;
-                  });
-                };
+              pythonOverrides = pyFinal: pyPrev: {
+                aioboto3 = pyPrev.aioboto3.overridePythonAttrs (old: {
+                  disabledTests = (old.disabledTests or [ ]) ++ [
+                    "test_dynamo_resource_query"
+                    "test_dynamo_resource_put"
+                    "test_dynamo_resource_batch_write_flush_on_exit_context"
+                    "test_dynamo_resource_batch_write_flush_amount"
+                    "test_flush_doesnt_reset_item_buffer"
+                    "test_dynamo_resource_property"
+                    "test_dynamo_resource_waiter"
+                  ];
+                });
+                fastmcp = pyPrev.fastmcp.overridePythonAttrs (old: {
+                  doCheck = false;
+                });
+              };
             in
             {
               python313Packages = prev.python313Packages.overrideScope pythonOverrides;
