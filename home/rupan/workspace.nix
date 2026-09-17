cc926a969314c5337759a33ae897da38e71a84c6
JEFF7712 <rupanpandyan@gmail.com>
2026-09-17 00:27:47 -0500

    feat(workspace): configure git identity, credentials, and difftastic

--- home/rupan/workspace.nix
+++ home/rupan/workspace.nix
@@ -24,4 +24,16 @@
   terminal.enable = true;
   dev.enable = true;
   cli-tools.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "JEFF7712";
+    userEmail = "rupanpandyan@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      credential."https://github.com".helper = "!gh auth git-credential";
+      credential."https://gist.github.com".helper = "!gh auth git-credential";
+      diff.external = lib.getExe pkgs.difftastic;
+    };
+  };
 }
